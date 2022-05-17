6d150e8dd9eb4b3b2f7b88b9eed1c0446140eb22
Kristian Dorland <kristian@dorland.dk>
2022-05-17 13:10:47 +0200

    Modul 14 eksempel og skabelon

---
+++ modul14/snippets/HashedPassword/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+// Brugeroplysninger
+string password = "password123";
+
+// Lav et 128-bit salt, som skal med i hash-funktionen når password hashes
+byte[] saltBytes = new byte[128 / 8];
+using (var rng = RandomNumberGenerator.Create())
+{
+    rng.GetNonZeroBytes(saltBytes);
+}
+string salt = Convert.ToBase64String(saltBytes);
+
+// Lav en 256-bit hash af (password + salt) - og gør det 100.000 gange!
+// HMACSHA256 er navnet på hash-funktionen
+string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+    password: password,
+    salt: Convert.FromBase64String(salt),
+    prf: KeyDerivationPrf.HMACSHA256,
+    iterationCount: 100000,
+    numBytesRequested: 256 / 8));
+
+Console.WriteLine($"Password {password} plus salt {salt} er hashed til {hashed}");
