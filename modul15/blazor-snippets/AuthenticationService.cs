3d171fe7a8412031b0de80a3a602c4b00e7e2d77
Kristian Dorland <kristian@dorland.dk>
2022-05-23 09:34:37 +0200

    Lidt snippets til modul 15

---
+++ modul15/blazor-snippets/AuthenticationService.cs
@@ -0,0 +1,55 @@
+using ElprisApp.Models;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace ElprisApp.Services
+{
+    public interface IAuthenticationService
+    {
+        User User { get; }
+        Task Initialize();
+        Task Login(string username, string password);
+        Task Logout();
+    }
+
+    public class AuthenticationService : IAuthenticationService
+    {
+        private record LoginResult(string token, string msg);
+
+        private IHttpService _httpService;
+        private NavigationManager _navigationManager;
+        private ILocalStorageService _localStorageService;
+
+        public User User { get; private set; }
+
+        public AuthenticationService(
+            IHttpService httpService,
+            NavigationManager navigationManager,
+            ILocalStorageService localStorageService
+        ) {
+            _httpService = httpService;
+            _navigationManager = navigationManager;
+            _localStorageService = localStorageService;
+        }
+
+        public async Task Initialize()
+        {
+            User = await _localStorageService.GetItem<User>("user");
+        }
+
+        public async Task Login(string username, string password)
+        {
+            LoginResult result = await _httpService.Post<LoginResult>("/api/users/login", new { username, password });
+            User = new User(username, result.token);
+            Console.WriteLine($"Username {username} and token {result.token}");
+            await _localStorageService.SetItem("user", User);
+        }
+
+        public async Task Logout()
+        {
+            User = null;
+            await _localStorageService.RemoveItem("user");
+            _navigationManager.NavigateTo("login");
+        }
+    }
+}
---
+++ modul15/blazor-snippets/ElPris.cs
@@ -0,0 +1,10 @@
+namespace ElprisApp.Models
+{
+    public class ElPris
+    {
+        public int Id { get; set; }
+        public DateTime HourDK { get; set; }
+        public string? SpotPriceDKK { get; set; }
+        public string? PriceArea { get; set; }
+    }
+}
